74e49922e753333f17f664f823cde5592959a71b
Aditya Mahendru <aditya.mahendru.17@gmail.com>
2020-11-11 00:10:57 +0530

    Added Error Handling to TestRunner

--- src/testrunner.py
+++ src/testrunner.py
@@ -3,16 +3,25 @@ import json
 
 def runTest(testCasePath,filesPath,filename):
 
-    p1=subprocess.Popen(['gcc',filesPath+filename,'-o',filesPath+'a.out'])
-    p1.communicate()
-
     with open(testCasePath, "r") as jfile:
         data = json.load(jfile)
     testResult = []
+
+    p1=subprocess.Popen(['gcc',filesPath+filename,'-o',filesPath+'a.out'],stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding='utf8')
+    if p1.communicate()[1].find('error:') != -1:
+        for cases in data["test_cases"]:
+            testResult.append(0)
+        return testResult
+
+    #print(p1.communicate()[1])
+    p1.communicate()
+
     for indata in data["test_cases"]:
         process = subprocess.Popen([filesPath+'a.out'], stdin=subprocess.PIPE,
-                                   stdout=subprocess.PIPE, encoding='utf8')
+                                   stdout=subprocess.PIPE,stderr=subprocess.PIPE, encoding='utf8')
         process.stdin.write(indata["test_case"])
-
-        testResult.append(1 if process.communicate()[0] == indata["output"] else 0)
+        if(process.communicate()[1]):
+            testResult.append(0)
+        else:
+            testResult.append(1 if process.communicate()[0] == indata["output"] else 0)
     return testResult
