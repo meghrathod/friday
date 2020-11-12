cf5fa839487c6206431f613287208c16fe067e68
Aditya Mahendru <aditya.mahendru.17@gmail.com>
2020-11-12 09:48:42 +0530

    Added custom ques number capability

--- src/extract.py
+++ src/extract.py
@@ -1,7 +1,15 @@
 from zipfile import ZipFile
 
-def extractAndList(filepath):
+def extractAndList(filepath,probNumber):
+    all_files=[]
     with ZipFile(filepath, 'r') as zipObj:
-        zipObj.extractall(filepath[:len(filepath)-4])
-        all_files = zipObj.namelist()
+        if probNumber:
+            file_names=zipObj.namelist()
+            for file in file_names:
+                if file.startswith(probNumber):
+                    zipObj.extract(file,filepath[:len(filepath)-4])
+                    all_files.append(file)
+        else:
+            zipObj.extractall(filepath[:len(filepath)-4])
+            all_files = zipObj.namelist()
     return all_files
